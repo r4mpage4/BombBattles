1e801ef56f3125c79ccd90f4cab9e6f17d83f49f
r4mpage4 <155855664+r4mpage4@users.noreply.github.com>
2024-12-18 10:50:57 +0800

    Delete BombBattles.lua

--- BombBattles.lua
+++
@@ -1,5 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/r4mpage4/UGCLIMITED/refs/heads/main/new.lua"))()
-
-wait(0.7)
-
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Lobby.Portal.Hit.CFrame
