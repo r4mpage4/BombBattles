034142b5c29d19ab3a7586e4e2808cc1db883a01
r4mpage4 <155855664+r4mpage4@users.noreply.github.com>
2024-12-18 10:44:29 +0800

    Update BombBattles.lua

--- BombBattles.lua
+++ BombBattles.lua
@@ -1,4 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/r4mpage4/UGCLIMITED/refs/heads/main/new.lua"))()
 
 wait(0.7)
+
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Lobby.Portal.Hit.CFrame
